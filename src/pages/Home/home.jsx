747a819d8b4d5f1c367375424cf836d8bc07b3d6
jnutterdev <hello@jnutterdev.com>
2022-08-21 09:38:39 -0400

    working on hero section

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { HereoSm, HeroMd, HeroLg } from '../../assets/'
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className="border-black border-x-2">
+            
+            <section>
+                <div className="container mx-auto gap-8 px-4 flex flex-col md:flex-row">
+                    <div className="md:flex-1 md:order-2">
+                        <picture>
+                            <source srcSet={HeroSm} width='363' height='222' media='(max-width: 400px)' type="image/jpg" />
+                            <source srcSet={HeroMd} width='363' height='222' media='(max-width: 400px)' type="image/jpg" />
+                            <img src={HeroLg} alt="hero image" width='870' height='532' alt='working hard on a computer' />
+                        </picture>
+                    </div>
+                    <div className="md:flex-1">Text</div>
+                </div>
+            </section>
+
+        </div>
     )
 }
 
